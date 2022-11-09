551b160b152a3ccd0bf71ffb1b56a67d2d2865eb
nicoelias997 <nicoelias997@gmail.com>
2022-11-09 19:40:41 +0100

    Completo el modelo Schema para los menues, y dejo comentado algunas cosas que podriamos agregar como mas detalle

--- src/models/menu.js
+++ src/models/menu.js
@@ -1,15 +1,53 @@
 import mongoose, {Schema} from "mongoose";
 
 const menuSchema = new Schema = {
-    nombreMenu: "Hamburguesa rustica",
-    precioMenu: 1400,
-    descripcion: "Hamburguesa 200gr de vacuno, con lechuga fresca y tomate de huerto, cebolla crispy. Con salsa especial de la casa. Acompañada de papas fritas o rusticas.",
-    imagen: "https://st2.depositphotos.com/1020618/8867/i/450/depositphotos_88671760-stock-photo-close-up-of-home-made.jpg",
-    pan: "brioche"
-  }
+    nombreMenu: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 5,
+        maxLength: 30
+    }, 
+    precioMenu: {
+        type: Number, 
+        required: true,
+        min: 100, 
+        max: 10000
+    }, 
+    descripcion: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 20,
+        maxLength: 1000
+    },
+    imagen: {
+        type: String,
+        required: true
+    },
+    pan:{
+        type: String, 
+        required: true
+    }    
+    // ,
+    // categoria: {
+    //     type: String,
+    //     required: true
+    // }
+    //podriamos agregar categoria si vendemos postres o entrantes, veremos como vamos con el tiempo
+}
 
-  const MENU = mongoose.model("menu", menuSchema)
+export const Menu = mongoose.model("menu", menuSchema)
 
+
+//   para el backend, manejando el administrador
+//   {
+//     nombreMenu: "Hamburguesa rustica",
+//     precioMenu: 1400,
+//     descripcion: "Hamburguesa 200gr de vacuno, con lechuga fresca y tomate de huerto, cebolla crispy. Con salsa especial de la casa. Acompañada de papas fritas o rusticas.",
+//     imagen: "https://st2.depositphotos.com/1020618/8867/i/450/depositphotos_88671760-stock-photo-close-up-of-home-made.jpg",
+//     pan: "brioche"
+//   }
 
 // para el FRONT END, POR SI ALGUIEN PIDE
 // {
