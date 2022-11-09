8942484a0b72a617f6823e36f5962586ae8e6769
matias cardoso <mattiascardoso10@gmail.com>
2022-11-09 10:39:02 -0300

    configure un puerto con express

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express"
+
+const app = express();
+
+
+app.set("port", process.env.PORT || 4000);
+
+app.listen( app.get("port"), ()=>{
+    console.log("estoy en el puerto"+ app.get("port"));
+});
+
+console.log("hola mundo")
