157c8d475c12b957bcd3a0e1005e911641b72b1d
matias cardoso <mattiascardoso10@gmail.com>
2022-11-09 11:21:41 -0300

    configure middlewares

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import path from "path"
 
 const app = express();
 
@@ -8,3 +11,17 @@ app.set("port", process.env.PORT || 4000);
 app.listen( app.get("port"), ()=>{
     console.log("estoy en el puerto"+ app.get("port"));
 });
+
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../public")));
+console.log(path.join(__dirname, "../public"))
+
+
+app.get("/", (req, res)=>{
+    res.send("Hola desde el backend en la peticion get")
+})
