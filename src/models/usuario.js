2365375abf80da2f9083a244b7bef01e398b9a07
nicoelias997 <nicoelias997@gmail.com>
2022-11-19 20:32:05 +0100

    Agrego un modelo para usuario siguiendo la estructura del frontEnd

---
+++ src/models/usuario.js
@@ -0,0 +1,30 @@
+import mongoose, {Schema} from "mongoose"
+
+const userSchema = new Schema({
+
+    nombreUsuario: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 30
+    }, 
+    apellidoUsuario: {
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 30
+    }, 
+    email: {
+        type: String,
+        required: true, 
+        unique: true
+    }, 
+    password: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 30
+    }
+})
+
+export const Usuario = mongoose.model("usuario", menuSchema)
