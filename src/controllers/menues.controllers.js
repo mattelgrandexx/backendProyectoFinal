ab5e73098abb6cb9948dee46423e56823ea81b34
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-11-24 18:28:15 -0300

    Peticiones y validaciones para los menus actualizadas

--- src/controllers/menues.controllers.js
+++ src/controllers/menues.controllers.js
@@ -1,36 +1,76 @@
-import { Hamburguesa } from "../models/menu"
+import { validationResult } from "express-validator";
+import { Menu } from "../models/menu";
 
+export const listarMenus = async (req, res) => {
+  try {
+    const menues = await Menu.find();
+    res.status(200).json(menues);
+  } catch (error) {
+    console.log(error);
+    res.staus(400).json({
+      message: "No pudimos encontrar los menus.",
+    });
+  }
+};
 
-export const listarMenues = async (req, res) => {
-    try{
-       const menues =  await Hamburguesa.find()
-        res.status(200).json(menues)
-
-    } catch(e){
-        res.staus(400).json({
-            message: "No pudimos encontrar los menues."
-        })
-    } 
-}
-
-export const crearMenues = async (req, res) => {
-    try{
-
-        const errors = validationResult(req)
-        if(!errors.isEmpty()){
-         return res.status(400).json({
-            errors: errors.array()
-          })
-        }
-
-        const nuevoMenu = new Menu(req.body)
-        await nuevoMenu.save()
-        res.status(201).json({
-            message: "Creamos el menu con exito."
-        })
-    } catch(e){
-        res.status(404).json({
-            message: "No pudimos crear el menu, intentenlo mas tarde."
-        })
+export const crearMenu = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
     }
-}
+
+    const nuevoMenu = new Menu(req.body);
+    await nuevoMenu.save();
+    res.status(201).json({
+      message: "El menu se creo con exito.",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      message: "No se pudo crear el menu, intentenlo mas tarde.",
+    });
+  }
+};
+
+export const obtenerMenu = async (req, res) => {
+  try {
+    const menuBuscado = await Menu.findById(req.params.id);
+    res.status(200).json(menuBuscado);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al buscar el menu especificado",
+    });
+  }
+};
+
+export const editarMenu = async (req, res) => {
+  try {
+    await Menu.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      mensaje: "El menu se actualizado correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      mensaje: "Error al editar el menu especificado",
+    });
+  }
+};
+
+export const borrarMenu = async (req, res) => {
+  try {
+    await Menu.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      mensaje: "El menu se borro correctamente",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al borrar el menu especificado",
+    });
+  }
+};
--- src/models/menu.js
+++ src/models/menu.js
@@ -1,65 +1,34 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
 
 const menuSchema = new Schema({
-    
-    nombreMenu: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: 5,
-        maxLength: 50
-    },
-    precioMenu: {
-        type: Number,
-        required: true,
-        min: 100,
-        max: 10000
-    },
-    descripcion: {
-        type: String,
-        required: true,
-        unique: true,
-        minLength: 20,
-        maxLength: 1000
-    },
-    imagen: {
-        type: String,
-        required: true
-    },
-    categoria:{
-        type: String,
-        required: true
-    }
-})
+  nombreMenu: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 4,
+    maxLength: 50,
+  },
+  precioMenu: {
+    type: Number,
+    required: true,
+    min: 100,
+    max: 10000,
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 10,
+    maxLength: 100,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+});
 
-    // ,
-    // categoria: {
-    //     type: String,
-    //     required: true
-    // }
-    //podriamos agregar categoria si vendemos postres o entrantes, veremos como vamos con el tiempo
-
-export const Hamburguesa = mongoose.model("menu", menuSchema)
-
-
-//   para el backend, manejando el administrador
-//   {
-//     nombreMenu: "Hamburguesa rustica",
-//     precioMenu: 1400,
-//     descripcion: "Hamburguesa 200gr de vacuno, con lechuga fresca y tomate de huerto, cebolla crispy. Con salsa especial de la casa. Acompañada de papas fritas o rusticas.",
-//     imagen: "https://st2.depositphotos.com/1020618/8867/i/450/depositphotos_88671760-stock-photo-close-up-of-home-made.jpg",
-//     pan: "brioche"
-//   }
-
-// para el FRONT END, POR SI ALGUIEN PIDE
-// {
-//     "nombreMenu": "Hamburguesa rustica",
-//     "precioMenu": 1400,
-//     "descripcion": "Hamburguesa 200gr de vacuno, con lechuga fresca y tomate de huerto, cebolla crispy. Con salsa especial de la casa. Acompañada de papas fritas o rusticas.",
-//     "imagen": "https://st2.depositphotos.com/1020618/8867/i/450/depositphotos_88671760-stock-photo-close-up-of-home-made.jpg",
-//     "pan": "brioche",
-//     "extras": ["cheddar", "bacon"],
-//     "coccion": "A punto",
-//     "detalle": "sin lechuga",
-//     "precioTotal": 1650
-//   }
+export const Menu = mongoose.model("menu", menuSchema);
