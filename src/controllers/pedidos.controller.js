10f62430363a2475b1920603089b81907da5f301
Luciano <lucasacci@hotmail.com>
2022-11-24 16:00:48 -0300

    controlador de pedidos

--- src/controllers/pedidos.controller.js
+++ src/controllers/pedidos.controller.js
@@ -43,7 +43,7 @@ export const editarPedidos =  async(req, res)=>{
         await Pedidos.findByIdAndUpdate(req.params.id, req.body);
 
         res.status(200).json({
-            mensaje:'Producto editado correctamente'
+            mensaje:'Pedido editado correctamente'
         })
 
     } catch (error) {
