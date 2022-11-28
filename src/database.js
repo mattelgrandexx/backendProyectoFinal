a2d1c765ca996023bff69f6a241898aad7a838ca
matias cardoso <mattiascardoso10@gmail.com>
2022-11-28 07:11:36 -0300

    nueva variable de entorno

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const url = "mongodb+srv://mati123:mati123@cluster0.2q6zsm3.mongodb.net/menu-bar"
+const url = "mongodb+srv://lenobar:lenobar4000@prueba.dhmudco.mongodb.net/menu-bar"
 
 mongoose.connect(url)
 
