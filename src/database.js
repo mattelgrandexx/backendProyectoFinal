ae9ae8f973595cc6b50de215ab9768f772cd5688
nicoelias997 <nicoelias997@gmail.com>
2022-11-30 14:08:13 +0100

    Cambio la url de la base de datos por la del mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb+srv://lenobar:lenobar4000@prueba.dhmudco.mongodb.net/menu-bar"
+// const url = "mongodb+srv://lenobar:lenobar4000@prueba.dhmudco.mongodb.net/menu-bar"
+const url = "mongodb+srv://nicoelias997:nicoelias997@cluster0.akvwdgj.mongodb.net/menu-bar"
 
 mongoose.connect(url)
 
