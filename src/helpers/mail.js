95b59620b41720bc7c9d985f2c14f567ed62b5bc
nicoelias997 <nicoelias997@gmail.com>
2022-11-30 15:29:04 +0100

    Cambio la url de la confirmacion del mail

--- src/helpers/mail.js
+++ src/helpers/mail.js
@@ -45,7 +45,7 @@ const mail = {
             <img src="https://i.postimg.cc/FFnKd1zQ/LENOLOGO3.png" alt="logo" heigth="400px" text-align="center" width="300px"></img>
             <p>Para confirmar tu cuenta, haz un click en el siguiente enlace</p>
             <a
-            href="https://shiny-meerkat-0b4284.netlify.app/confirmar/${token}"
+            href="https://musical-syrniki-620ff4.netlify.app/confirmar/${token}"
             target="_blank">
             Confirmar Email</a>
            </div>
