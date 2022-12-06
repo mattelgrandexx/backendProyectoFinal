8142c353bcf12a1a55570d2080193fc7c546fb28
nicoelias997 <nicoelias997@gmail.com>
2022-12-06 23:36:10 +0100

    Ya que cambiamos la url de nuestra pagina, cambio tambien los href de los links de los email que envia leno para recuperarPass y confirmarEmail

--- src/helpers/mail.js
+++ src/helpers/mail.js
@@ -45,7 +45,7 @@ const mail = {
             <img src="https://i.postimg.cc/FFnKd1zQ/LENOLOGO3.png" alt="logo" heigth="400px" text-align="center" width="300px"></img>
             <p>Para confirmar tu cuenta, haz un click en el siguiente enlace</p>
             <a
-            href="https://musical-syrniki-620ff4.netlify.app/confirmar/${token}"
+            href="https://lenohamburguesas.netlify.app/confirmar/${token}"
             target="_blank">
             Confirmar Email</a>
            </div>
@@ -59,7 +59,7 @@ const mail = {
           <img src="https://i.postimg.cc/FFnKd1zQ/LENOLOGO3.png" alt="logo" heigth="400px" text-align="center" width="300px"></img>
           <p>Para recuperar tu contraseña, haz un click en el siguiente enlace</p>
           <a
-  href="https://musical-syrniki-620ff4.netlify.app/reset/${token}"
+  href="https://lenohamburguesas.netlify.app/reset/${token}"
           target="_blank">
           Recuperar contraseña</a>
          </div>
